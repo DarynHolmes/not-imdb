598b8032093c42760a5e9a84c4baaee48429ba5c
Daryn Holmes <daryn.holmes@insightsoftware.com>
2017-07-14 17:36:06 +0100

    adding review count

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -13,16 +13,21 @@
             ></v-text-field>
           </v-flex>
        </v-layout>
-      <div v-for="(film, index) in films" :key="film.id" @click="filmClicked(film)">
-                <v-list-tile avatar v-bind:key="film.title">
-                <v-list-tile-avatar>
-                    <img v-bind:src="film.thumbnail"></v-list-tile-avatar>
-                </v-list-tile-avatar>
-                <v-list-tile-content>
-                    <v-list-tile-title v-html="film.title"></v-list-tile-title>
-                </v-list-tile-content>
+      <!-- <div > -->
+                <v-list-tile v-for="film in films" :key="film.id" @click.native="filmClicked(film)" avatar v-bind:key="film.title">
+                    <v-list-tile-avatar>
+                        <img v-bind:src="film.thumbnail"></v-list-tile-avatar>
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-html="film.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                    <v-list-tile-action>
+                      <div class="text-xs-center">
+                          <v-chip>{{film.reviews.length}}</v-chip>
+                      </div>
+                    </v-list-tile-action>
                 </v-list-tile>
-        </div>
+        <!-- </div> -->
     </v-list>
 
 </template>
@@ -38,6 +43,7 @@
     },
     methods: {
       filmClicked(film) {
+        console.log("Clicked!!!");
         this.$emit('filmClicked', film)
       },
       setSearch: debounce(function (term) {
--- src/components/FilmTemplate.vue
+++ src/components/FilmTemplate.vue
@@ -30,11 +30,7 @@
     },
     data () {
       return {
-        films: [
-          // { id: 1, thumbnail: 'http://i.dailymail.co.uk/i/pix/2017/01/16/20/332EE38400000578-4125738-image-a-132_1484600112489.jpg', title: 'Dinner this weekend?', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-          // { id: 2, thumbnail: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlUiyrmhTXFppqk4aYzqTOU9nimCQsYibukwAV8rstsDkAVQT-mA', title: 'Summer BBQ', subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend." },
-          // { id: 3, thumbnail: 'http://i.dailymail.co.uk/i/pix/2016/09/13/14/384A98B300000578-0-image-a-63_1473774825844.jpg', title: 'Oui oui', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?" }
-        ],
+        films: [],
         selectedFilm: null,
         isLoading: false
       }
@@ -46,7 +42,6 @@
     },
     methods: {
       selectFilm(film) {
-        // console.log(film);
         this.selectedFilm = film;
       },
       newSearch(searchTerm) {
@@ -57,9 +52,8 @@
 
         api.searchByTitle(searchTerm)
           .then(response => {
-            // console.log(data);
             this.films = response.data.films
-            })
+          })
           .catch(console.error);
       }
     }
